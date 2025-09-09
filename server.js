5b92de4b3c7f37296fbbccdd5278fe0f36eef68f
MortOP33 <laurent.chaye@gmail.com>
2025-09-09 23:00:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// test
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 let game = {};
